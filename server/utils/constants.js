0d4ec30c6763bd294cbf5e4a2141168a8b5f8c94
RanganadhY <ranganadhyadlapalli@outlook.com>
2022-12-07 22:40:49 +0530

    pushing cors

--- server/utils/constants.js
+++ server/utils/constants.js
@@ -2,4 +2,4 @@ const branchCodes = ["CS","AD","CV","IS","ML","BT","ME","IM","CH","EE","EC","EI"
 
 const semesters = [1,2,3,4,5,6,7,8];
 
-module.exports = {branchCodes, semesters};
+module.exports = {branchCodes, semesters}
