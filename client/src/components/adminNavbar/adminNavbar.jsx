326fa6a28e6d86803749e5543bc52a72e847cf6f
Anudeep <anudeep.hanuman@gmail.com>
2022-12-07 10:33:13 +0530

    better loading of settings in admin panel

--- client/src/components/adminNavbar/adminNavbar.jsx
+++ client/src/components/adminNavbar/adminNavbar.jsx
@@ -1,21 +1,19 @@
-import React,{useState} from 'react'
+import React from 'react'
 import {NavLink,Link, useNavigate} from "react-router-dom"
 
 //importing stylesheet
 import "../adminNavbar/adminnavbar.css"
 
 import axios from "../../axios/axios";
-import { AdminLoader } from '../loading component/loader';
 
 function AdminNavbar() {
 
     const navigate = useNavigate();
-    const [loading, setLoading] = useState(false);
 
     let status;
 
     const getSubjectSelectionStatus = async() => {
-        setLoading(true);
+        
         const res = await axios.get("/admin/get-status")
                                 .catch((err)=>{
                                     console.log(err);
@@ -23,19 +21,16 @@ function AdminNavbar() {
                                 });
         if(res.data){
             if(res.data.message === "successfull"){
+                navigate("/selection-enable-disable",{state:{status:res.data.status}})
                 status=res.data.status;
             }else{
                 alert(res.data.message)
             }
         }
-        setLoading(false);
     }
 
     return (
         <>
-        {
-            loading && <AdminLoader/>
-        }
             <div className="adminNavabar-main-conatiner">
                 <div className="adminNavbar-collage-heading">
                     <h2>Siddaganga Institute Of Technology</h2>
@@ -45,7 +40,7 @@ function AdminNavbar() {
                     <li><NavLink to="/modify-subjects">Subject Mgmt</NavLink></li>
                     <li><NavLink to="/mapping">Mapping</NavLink></li>
                     <li><NavLink to="/report">Report</NavLink></li>
-                    <li onClick={getSubjectSelectionStatus}><NavLink to="/selection-enable-disable" state={{status:status}}>Settings</NavLink></li>
+                    <li onClick={getSubjectSelectionStatus}> <NavLink>Settings</NavLink></li>
                 </ul>
             </div>
             
