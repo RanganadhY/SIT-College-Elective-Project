7f9c6ecf6d708ad8e354cf6fb7a90b6e28db3b5d
Anudeep <anudeep.hanuman@gmail.com>
2022-12-07 08:10:28 +0530

    better loading of settings in admin panel

--- client/src/components/adminNavbar/adminNavbar.jsx
+++ client/src/components/adminNavbar/adminNavbar.jsx
@@ -1,12 +1,41 @@
-import React from 'react'
-import {NavLink,Link} from "react-router-dom"
+import React,{useState} from 'react'
+import {NavLink,Link, useNavigate} from "react-router-dom"
 
 //importing stylesheet
 import "../adminNavbar/adminnavbar.css"
 
+import axios from "../../axios/axios";
+import { AdminLoader } from '../loading component/loader';
+
 function AdminNavbar() {
+
+    const navigate = useNavigate();
+    const [loading, setLoading] = useState(false);
+
+    let status;
+
+    const getSubjectSelectionStatus = async() => {
+        setLoading(true);
+        const res = await axios.get("/admin/get-status")
+                                .catch((err)=>{
+                                    console.log(err);
+                                    alert("something went wrong. Please try again later.");
+                                });
+        if(res.data){
+            if(res.data.message === "successfull"){
+                status=res.data.status;
+            }else{
+                alert(res.data.message)
+            }
+        }
+        setLoading(false);
+    }
+
     return (
         <>
+        {
+            loading && <AdminLoader/>
+        }
             <div className="adminNavabar-main-conatiner">
                 <div className="adminNavbar-collage-heading">
                     <h2>Siddaganga Institute Of Technology</h2>
@@ -16,7 +45,7 @@ function AdminNavbar() {
                     <li><NavLink to="/modify-subjects">Subject Mgmt</NavLink></li>
                     <li><NavLink to="/mapping">Mapping</NavLink></li>
                     <li><NavLink to="/report">Report</NavLink></li>
-                    <li><NavLink to="/selection-enable-disable">Settings</NavLink></li>
+                    <li onClick={getSubjectSelectionStatus}><NavLink to="/selection-enable-disable" state={{status:status}}>Settings</NavLink></li>
                 </ul>
             </div>
             
